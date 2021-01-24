c44aad9132e7fb3164dc4cd15772b73a09503f36
Ruth Sperer <ruth@trekace.com>
2021-01-24 15:19:44 +0200

    add canonical URL extraction and image properties to response

--- flaskog/og_parse.py
+++ flaskog/og_parse.py
@@ -1,47 +1,60 @@
+import json
+import logging
+from datetime import datetime
+
 from bs4 import BeautifulSoup
 import requests
+from opengraph_parse import parse_page
 
-# copied from opengraph_parse and modified for exercise purposes
-# By no means is this a complete list, but it is very easy to search for the ones you need later.
-KNOWN_OPENGRAPH_TAGS = [
-    "og:site_name",
-    "og:title",
-    "og:locale",
-    "og:type",
-    "og:image",
-    "og:url",
-    "og:image:url",
-    "og:image:secure_url",
-    "og:image:type",
-    "og:image:width",
-    "og:image:height",
-    "og:image:alt",
-    ]
+from flaskog import db
+from flaskog.models import OGP
 
 
-def parse_page(page_url, tags_to_search=KNOWN_OPENGRAPH_TAGS):
-    """
-    Parses a page, returns a JSON style dictionary of all OG tags found on that page.
-
-    Passing in tags_to_search is optional. By default it will search through KNOWN_OPENGRAPH_TAGS constant, but for the sake of efficiency, you may want to only search for 1 or 2 tags
-
-    Returns False if page is unreadable
-    """
-    # read the html from the page
+def get_canonical(page_url: str) -> str:
     response = requests.get(page_url)
-
     if response.status_code is not 200:
-        return False
+        return page_url
 
-    # set up beautiful soup
     soup = BeautifulSoup(response.content, 'html.parser')
+    canonical = soup.find("link", rel="canonical")
+    if canonical:
+        value = canonical["href"]
+        logging.debug(f"found using rel: {value}")
+        return value
 
-    # loop through the known list of opengraph tags, searching for each and appending a dictionary as we go.
-    found_tags = {}
+    canonical = soup.find("meta", property="og:url")
+    if canonical:
+        value = canonical["content"]
+        logging.debug(f"found using og:url: {value}")
+        return value
 
-    for og_tag in tags_to_search:
-        new_found_tag = soup.find("meta",  property=og_tag)
-        if new_found_tag is not None:
-            found_tags[new_found_tag["property"]] = new_found_tag["content"]
+    return page_url
 
-    return found_tags
+
+def scrape_og_tags(url, url_id):
+    logging.debug(f"start scraping {url}")
+    record = OGP.query.filter_by(url_id=url_id).first()
+    if not record:
+        return
+    content = {"id": str(url_id), "url": url}
+    image = {}
+    og_tags = parse_page(url)
+    if og_tags:
+        # convert og tags to our tags
+        for key, value in og_tags.items():
+            clean_key = key.replace("og:", "")
+            if clean_key == "image":
+                image["url"] = value
+            elif clean_key.startswith("image"):
+                image[clean_key.replace("image:", "")] = value
+            else:
+                content[clean_key] = value
+        if image:
+            content["images"] = [image]
+        content["scrape_status"] = "done"
+    else:
+        content["scrape_status"] = "error"
+    content["updated_time"] = str(datetime.now())
+    record.json = json.dumps(content, default=str)
+    db.session.commit()
+    logging.debug(f"saving {url}")
--- flaskog/routes.py
+++ flaskog/routes.py
@@ -1,20 +1,11 @@
-import logging
 import threading
-
 from flask import request
 from flaskog import app
 from flaskog.models import *
-from flaskog.og_parse import parse_page
+from flaskog.og_parse import *
 import json
 
-
-@app.route("/")
-def hello():
-    return "<h1>Hello Me!</h1>"
-
-
-def get_canonical(input_url: str):
-    return input_url
+from flaskog.og_parse import scrape_og_tags
 
 
 @app.route("/stories", methods=["POST"])
@@ -43,7 +34,8 @@ def scrape_url_id(url_id: int):
     if ogp:
         return ogp.json
     # This is the first time we've been asked to scrape this URL
-    content = {"id": str(url_id), "url": url.canonical_url, "scrape_status": "pending"}
+    content = {"id": str(url_id), "url": url.canonical_url,
+               "scrape_status": "pending", "updated_time": str(datetime.now())}
     json_response = json.dumps(content, default=str)
     db.session.add(OGP(url_id=url_id, json=json_response))
     db.session.commit()
@@ -51,21 +43,3 @@ def scrape_url_id(url_id: int):
     return json_response
 
 
-def scrape_og_tags(url, url_id):
-    logging.info(f"start scraping {url}")
-    record = OGP.query.filter_by(url_id=url_id).first()
-    if not record:
-        return
-    content = {"id": str(url_id), "url": url}
-    og_tags = parse_page(url)
-    if og_tags:
-        # convert og tags to our tags
-        for key, value in og_tags.items():
-            clean_key = key.replace("og:", "")
-            content[clean_key] = value
-        content["scrape_status"] = "done"
-    else:
-        content["scrape_status"] = "error"
-    record.json = json.dumps(content, default=str)
-    db.session.commit()
-    logging.info(f"saving {url}")
