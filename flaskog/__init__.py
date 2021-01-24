4fe0a462aede07c1553acb312d847466f5ee4c5f
Ruth Sperer <ruth@trekace.com>
2021-01-24 15:18:50 +0200

    set logging level to debug

--- flaskog/__init__.py
+++ flaskog/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import logging
+
 app = Flask(__name__)
+
+logging.basicConfig(level=logging.DEBUG)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
